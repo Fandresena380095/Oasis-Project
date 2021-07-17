e2ce7ee9a538019353396cdbb2969c13a4821ab3
Ny Avo <Jeandelacroix38@gmail.com>
2021-07-17 13:49:52 +0300

    Lazy Changes2 , not even finished

--- oasisApp/models.py
+++ oasisApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import User 
 
 # Create your models here.
+class Activity(models.Model):
+	CATEGORY = (
+		('POOL','POOL'),
+		('BAR','BAR'),
+		('CHILL_ROOM','CHILL_ROOM'),
+		('RESTAURANT','RESTAURANT'),
+		('ROOMS','ROOMS'),
+		('HAMAM','HAMAM'),
+		('MASSAGE_ROOM','MASSAGE_ROOM')
+		)
+
+	activity_name = models.CharField(max_length=300)
+	activity_description = models.CharField(max_length=300 ,null= True)
+	category = models.CharField(max_length=300, choices=CATEGORY, null=True)
+
+	def __str__(self):
+		return self.activity_name
+
+class Client(models.Model):
+	
