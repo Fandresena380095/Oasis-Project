7fdce7812a95e66872ba6a03e9661bf87a87a5d5
Ny Avo <Jeandelacroix38@gmail.com>
2021-07-17 15:17:15 +0300

    Lazy commit

--- oasisApp/admin.py
+++ oasisApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Client_Database)
+admin.site.register(Staff_Database_Access)
+admin.site.register(Activity)
--- oasisApp/models.py
+++ oasisApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth import User 
+from django.contrib.auth.models import User 
 
 # Create your models here.
 class Activity(models.Model):
@@ -20,5 +20,35 @@ class Activity(models.Model):
 	def __str__(self):
 		return self.activity_name
 
+
 class Client(models.Model):
-	
+	name = models.CharField(max_length=300)
+	email = models.CharField(max_length= 300, null=True)
+	date_registered = models.DateTimeField(auto_now_add=True , null= True)
+
+	def __str__(self):
+		return self.name
+
+
+class Client_Database(models.Model):
+	date_registered = models.DateTimeField(auto_now_add=True , null= True)
+	client = models.ForeignKey(User, null=True , on_delete=models.SET_NULL)
+	activities = models.ForeignKey(Activity,null=True, on_delete=models.SET_NULL)
+
+	def __str__(self):
+		return self.activities
+
+
+class Staff_Database_Access(models.Model):
+	date_registered = models.DateTimeField(auto_now_add=True , null= True)
+	client = models.ForeignKey(Client, null=True , on_delete=models.SET_NULL)
+	activities = models.ForeignKey(Activity,null=True, on_delete=models.SET_NULL)
+
+	def __str__(self):
+		return f'Client {self.client}:Activity {self.activities}:Booked at {sefl.date_registered}'
+
+
+
+
+
+
